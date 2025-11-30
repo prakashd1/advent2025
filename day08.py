d1879b15436f04d16ed78c7b368faede5def02eb
prakash <prakash.dayaramani@gmail.com>
2025-11-30 23:38:49 +0530

    Added templates

---
+++ day08.py
@@ -0,0 +1,12 @@
+from collections import defaultdict, deque
+def parse(file_path):
+    with open(file_path, "r") as file:
+        content = file.read().strip()
+
+
+def part1():
+    pass
+
+def part2():
+    pass
+
