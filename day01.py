f0611982628712a31c8f841275d7e235e88a0bf4
prakash <prakash.dayaramani@gmail.com>
2025-12-01 15:04:35 +0530

    day 1 final commit

--- day01.py
+++ day01.py
@@ -1,20 +1,15 @@
 from collections import defaultdict, deque
 
-
 def parse():
     with open("data/day01.txt", "r") as file:
         return file.readlines()
-
 
 def part12():
     curr = 50
     part1, part2 = 0, 0
     for x in inputs:
-        x = x.strip()
-        direction = x[:1]
-        clicks = int(x[1:])
-        factor = -1 if direction == "L" else 1
-        curr, z = calc(curr, clicks * factor)
+        direction, clicks = x[:1],  int(x[1:])
+        curr, z = calc(curr, clicks * (-1 if direction == "L" else 1))
         part1 += (1 if curr == 0 else 0)
         part2 += (1 if curr == 0 else 0) + z
         # print(f"{x} Curr {curr} and {(1 if curr == 0 else 0)} {z}")
@@ -22,17 +17,11 @@ def part12():
     print(part2)
 
 
-
-
 def calc(curr, movement):
-    z = 0
-    new_pos = (curr + movement) % 100
-    if curr == 0 and movement < 0:
-        curr = 100
-
+    z, new_pos = 0 , (curr + movement) % 100
+    curr = 100 if (curr == 0 and movement < 0) else curr
+    z -= 1 if (new_pos == 0 and movement > 0) else 0
     z += abs((curr + movement) // 100)
-    if new_pos == 0 and movement > 0:
-        z -= 1
     return new_pos, z
 
 inputs = parse()
